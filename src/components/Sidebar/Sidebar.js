88982163f172ee87db160384b59cbbe906b1357d
Michael (Daichi) Yanagi <6637846+dyanagi@users.noreply.github.com>
2019-12-14 09:24:09 -0800

    Add a sidebar header

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import SidebarHeader from './SidebarHeader'
 
 function Sidebar () {
   return (
     <div className='Sidebar'>
-      Sidebar
+      <SidebarHeader />
+      <div>
+        <p>All Notes</p>
+      </div>
+      <div>
+        <p>TODOs</p>
+        <p>Web Design</p>
+        <p>Programming Tips & Tricks</p>
+        <p>＋ Add a notebook</p>
+      </div>
     </div>
   )
 }
---
+++ src/components/Sidebar/SidebarHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewIconButton from '../Common/icon_buttons/NewIconButton'
+import ActionMenuIconButton from '../Common/icon_buttons/ActionMenuIconButton'
+
+function SidebarHeader (props) {
+  return (
+    <div>
+      <NewIconButton />
+      <ActionMenuIconButton />
+    </div>
+  )
+}
+
+export default SidebarHeader
